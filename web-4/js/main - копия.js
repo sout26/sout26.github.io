4d5d30d20f797dc99e0dd3e2da2bedf9f9fdfa81
sout26 <sout26@mail.ru>
2017-04-06 14:07:11 +0300

    Added web-4

---
+++ web-4/js/main - копия.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	// $.getScript("wow.min.js"); 
+	// new WOW().init();
+	
+});
+
+
+// jQuery(document).ready(function ($) { 
+	// $.getScript("wow.min.js"); 
+	// new WOW().init();
+// })(jQuery);
+
+
+
+// $(function() {
+// 	alert('Привет');
+// });
+
+// $.getScript('http://example.com/script.js', function(){
+//   alert('script loaded');
+// });
+
+
+// jQuery(document).ready(function ($) { 
+// 	$.getScript("wow.min.js"); 
+// 	new WOW().init();
+// })(jQuery);
+
+// $(Document).ready(function() {
+// 	$.getScript('wow.min.js', function(){
+// 		new WOW().init();
+// 	});
+	
+// });
+
---
+++ web-4/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+	// Smooth scroll to the pages block
+	// ==========================================================
+	$('#bs-example-navbar-collapse-1 a').on('click', function(e) {
+		e.preventDefault();
+
+	});
+});
